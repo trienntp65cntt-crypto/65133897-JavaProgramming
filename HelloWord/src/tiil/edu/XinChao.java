504c8d700ddefb4e89ae3ea25f9099368d211a20
trienntp65cntt-crypto <trien.ntp.65cntt@ntu.edu.vn>
2026-01-22 23:52:55 +0700

    ví dụ đầu tiên
    demo

---
+++ HelloWord/src/tiil/edu/XinChao.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tiil.edu;
+
+/**
+ * 
+ */
+public class XinChao {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" tôi lần đâu học java");
+
+	}
+
+}
