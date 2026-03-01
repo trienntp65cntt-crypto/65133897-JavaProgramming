e13d035069561f96e5aad3ae51dc5cf97492c867
trienntp65cntt-crypto <trien.ntp.65cntt@ntu.edu.vn>
2026-03-01 23:35:50 +0700

    thêm lệnh

---
+++ BaiTap1/src/BaiTap.java
@@ -0,0 +1,16 @@
+
+public class BaiTap {
+
+	public static void main(String[] args) {
+		// khai báo các biến
+		int a =15;
+		int b = 4;
+		double c = 3,5;
+		// tính toán
+		int tong = a+b;
+		double thuong = a/b;
+		
+		
+	}
+
+}
